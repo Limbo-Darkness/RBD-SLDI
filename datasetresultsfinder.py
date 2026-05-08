6d7d62cd58cb1fe4ea6d3c1b6ce72c11bc864bbf
Limbo-Darkness <jaydenrlopez@gmail.com>
2026-05-07 21:05:27 -0400

    Clean datasetresultsfinder.py

--- datasetresultsfinder.py
+++ datasetresultsfinder.py
@@ -4,6 +4,7 @@ from pathlib import Path
 # gather images
 imagedirectory = Path('image-loader')
 data = []
+loadcounter = 0
 for images in imagedirectory.iterdir():
     imagename = images.name
 
@@ -14,8 +15,12 @@ for images in imagedirectory.iterdir():
         continue
     else:
         data.append(str(images))
+        loadcounter+=1
+
+print(f'{loadcounter} images loaded.')
 
 # iterating through all 120 scenarios
+# 6 different preprocessors
 for i in range(0,6):
     if i == 0:
         pre = 'None'
@@ -29,6 +34,7 @@ for i in range(0,6):
         pre = 'Histogram Equalization'
     elif i == 5:
         pre = 'CLAHE'
+    # 5 different degradations
     for j in range(0,5):
         if j == 0:
             deg = 'None'
@@ -40,6 +46,7 @@ for i in range(0,6):
             deg = 'Blur'
         elif j == 4:
             deg = 'Reduce Illumination'
+        #4 different edge detectors
         for k in range(0,4):
             if k == 0:
                 edg = 'Sobel'
@@ -49,15 +56,8 @@ for i in range(0,6):
                 edg = 'LoG'
             elif k == 3:
                 edg = 'Canny'
+            # assemble code from iterative loop
             code = f'{i}{j}{k}'
-            # skip already completed codes
-            if code in ['000','010','020','030','040','100','110','120','001','011','002','003','012','013',
-                        '021','022','023','031','032','033','041','042','043','101','102','103','111','112',
-                        '113','121','122','123','130','131','132','133','140','141','142','143','200','201',
-                        '202','203','210','211','212','220','221','222','230','231','232','240','510','511',
-                        '512','520','521','522','530','531','532','540','541','542']:
-                continue
-            if k == 3:
-                continue
             print(f'Code: {code}')
+            # execute calculation step for all settings
             result.calculate(data,pre,deg,edg)
